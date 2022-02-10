0907b88f9a40d68e554b91a001b2cdd6fb3071b6
smth324 <Isaerwilliamvicsongocheco@gmail.com>
2022-02-10 13:48:40 +0800

    test change #skip

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ app.use(express.json())
 app.get('/health', (_req,res) => {
   res.send('ok')
 })
-
+// changes does this work
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
-//comment change
+
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
