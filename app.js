a84e0254167cbbd48dd1d3bf665669c13df129e1
smth324 <Isaerwilliamvicsongocheco@gmail.com>
2022-02-10 16:29:40 +0800

    private tele

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json())
 app.get('/health', (_req,res) => {
   res.send('ok')
 })
-// changes does this work
+// changes does this works
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
